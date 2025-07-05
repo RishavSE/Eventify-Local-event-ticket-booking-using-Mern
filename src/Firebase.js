f037261d0715d451edb39df44c2063e03bde1e74
Rishav Kumar Singh <rishavse06@gmail.com>
2025-07-05 20:55:48 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database"; // ✅ Realtime DB
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3o7Y1rKgRkualsRc1zwH66mvlkXsewtc",
+  apiKey: "Enter_your_api",
   authDomain: "local-ticket.firebaseapp.com",
   projectId: "local-ticket",
   storageBucket: "local-ticket.firebasestorage.app",
-  messagingSenderId: "862320605976",
-  appId: "1:862320605976:web:eac82bffa8df7d8b463c6d"
+  messagingSenderId: "Enter_your_data",
+  appId: "enter_your_data"
 };
 
 // ✅ Initialize Firebase
